02647c96ff392be2d07e7e45b3918a08b4e513a9
Human <95001357+Humanooooo@users.noreply.github.com>
2026-03-02 11:46:16 +0000

    v1.3

---
+++ ExercícioStrings.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int idade, qtdPessoas = 0, soma = 0;
+    double media;
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+    while (idade >= 0) {
+        qtdPessoas += 1;
+        soma += idade;
+        printf("Digite sua idade (<0 p/ finalizar): ");
+        scanf("%d", &idade);
+    }
+    if (qtdPessoas != 0) {
+        media = (double) soma / (double) qtdPessoas;
+        printf("A média de idade dos participantes é de %.2lf anos\n", media);
+    } else {
+        printf("Nenhuma idade válida foi digitada!\n");
+    }
+    return 0;
+}
+
+void MediaIdadeComStrings() {
+    char idadeStr[10];
+    int idade, qtdPessoas = 0, soma = 0;
+    double media;
+    printf("Digite sua idade: ");
+    scanf("%s", idadeStr);
+    while (idadeStr[0] != '-') {
+        idade = atoi(idadeStr);
+        if (idade >= 0) {
+            qtdPessoas += 1;
+            soma += idade;
+        }
+        printf("Digite sua idade (<0 p/ finalizar): ");
+        scanf("%s", idadeStr);
+    }
+    if (qtdPessoas != 0) {
+        media = (double) soma / (double) qtdPessoas;
+        printf("A média de idade dos participantes é de %.2lf anos\n", media);
+    } else {
+        printf("Nenhuma idade válida foi digitada!\n");
+    }
+}
